00f28cf7d4fd12f739928a6442320c9ac0214e8a
Arty <artefolei@gmail.com>
2023-03-19 07:52:49 +0100

    dostylována responzivní navigace

--- js/obsluha.js
+++ js/obsluha.js
@@ -4,12 +4,10 @@ const hamburgerNav = document.querySelector("#hamburger-nav");
 
 hamburgerBtn.addEventListener("click", function () {
     xMarkBtn.style.display = "flex";
-    hamburgerBtn.style.display = "none"
     hamburgerNav.style.display = "flex";
   });
   
   xMarkBtn.addEventListener("click", function(){
-      hamburgerBtn.style.display = "flex"
       xMarkBtn.style.display = "none"
       hamburgerNav.style.display = "none"
   })
